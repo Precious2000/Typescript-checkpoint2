f9320108903fc730e784428bf0723fc50c0cbe2f
Precious Oluwagbenga <121139863+Precious2000@users.noreply.github.com>
2024-10-10 16:04:22 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import Counter from "./Typescript/Counter"
+import Greeting from "./Typescript/Greeting"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Greeting name={"ibrahim"} />
+    <Greeting name={"Tunde"} />
+    <Counter />
+    
+    </>
+  )
+}
+
+export default App
